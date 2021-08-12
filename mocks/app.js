b968e3e3d238b1964171dfd21e6975b845c1ce0b
末烛 <1822984948@qq.com>
2021-08-12 16:55:25 +0800

    fix: 修改mock服务器过时API

--- mocks/app.js
+++ mocks/app.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const http = require('http');
-const bodyParser = require('body-parser');
 
 const PORT = 3000;
 const app = express();
 const server = http.createServer(app);
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/api', require('./controllers'));
 
 server.listen(PORT);
