8d6a386401d0c062d6c1d6420e9f4caf6e6d4870
末烛 <1822984948@qq.com>
2022-09-03 00:43:40 +0800

    fix: 使用normalize.css替换reseter.css

--- scripts/loaders/css2var-loader.js
+++ scripts/loaders/css2var-loader.js
@@ -1,12 +1,3 @@
-function getRootCssVar(colorMap) {
-    const cssVars = Object.keys(colorMap).reduce((prev, color) => {
-        return `${prev}--${color.replace(/([A-Z])/g, '-$1').toLowerCase()}: ${colorMap[color]};`;
-    }, '');
-    return `:root {
-        ${cssVars}
-    }`;
-}
-
 /**
  * css颜色转变为cssVar
  * @param {string|Buffer} content 就是传入的源内容字符串
@@ -15,12 +6,11 @@ function getRootCssVar(colorMap) {
 module.exports = function (content) {
     let data = content;
     const { cssVarMap = {} } = this.getOptions();
-    const rootVar = getRootCssVar(cssVarMap);
     Object.keys(cssVarMap).forEach((key) => {
         data = data.replace(
             new RegExp(`(${cssVarMap[key]})`, 'ig'),
             `var(--${key.replace(/([A-Z])/g, '-$1').toLowerCase()}, $1)`
         );
     });
-    return rootVar + data;
+    return data;
 };
