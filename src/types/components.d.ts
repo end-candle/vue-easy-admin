d9e6262804183351b66d941983c3ea6b0b5ab61b
end-candle <1822984948@qq.com>
2024-09-01 21:32:49 +0800

    fix: 修复构建报错

--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -27,6 +27,7 @@ declare module 'vue' {
     ElHeader: typeof import('element-plus/es')['ElHeader']
     ElInput: typeof import('element-plus/es')['ElInput']
     ElLink: typeof import('element-plus/es')['ElLink']
+    ElLoading: typeof import('element-plus/es')['ElLoading']
     ElMain: typeof import('element-plus/es')['ElMain']
     ElMenu: typeof import('element-plus/es')['ElMenu']
     ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
